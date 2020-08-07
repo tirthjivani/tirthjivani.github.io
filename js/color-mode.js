b280b948a5b62423c663a68cfca799731bd3056a
Tirth Jivani <tirthjivani17@gmail.com>
2020-08-07 09:35:04 +0530

    dark mode added

---
+++ js/color-mode.js
@@ -0,0 +1,17 @@
+console.log("infunction");
+const btn = document.querySelector(".check");
+const currentTheme = localStorage.getItem("theme");
+
+if (currentTheme == "dark") {
+  document.body.classList.add("dark-theme");
+}
+
+btn.addEventListener("click", function () {
+  document.body.classList.toggle("dark-theme");
+
+  let theme = "light";
+  if (document.body.classList.contains("dark-theme")) {
+    theme = "dark";
+  }
+  localStorage.setItem("theme", theme);
+});
