92d78c4681b56e876b7dc4d4931f52eae57bcb42
Tirth Jivani <tirthjivani17@gmail.com>
2020-08-15 19:45:54 +0530

    dark mode button modified

--- js/color-mode.js
+++ js/color-mode.js
@@ -1,9 +1,9 @@
-console.log("infunction");
-const btn = document.querySelector(".check");
+const btn = document.querySelector(".dark");
 const currentTheme = localStorage.getItem("theme");
 
 if (currentTheme == "dark") {
   document.body.classList.add("dark-theme");
+  btn.innerHTML = '<i class="fas fa-sun fa-2x" style="color: #fff;"></i>';
 }
 
 btn.addEventListener("click", function () {
@@ -12,6 +12,9 @@ btn.addEventListener("click", function () {
   let theme = "light";
   if (document.body.classList.contains("dark-theme")) {
     theme = "dark";
+    btn.innerHTML = '<i class="fas fa-sun fa-2x" style="color: #fff;"></i>';
+  } else {
+    btn.innerHTML = '<i class="fas fa-moon fa-2x" style="color: #000;"></i>';
   }
   localStorage.setItem("theme", theme);
 });
