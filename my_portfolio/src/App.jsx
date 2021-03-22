dd6a7bfbe8c7d27c9715ec8cb6929244a06ccbf0
Tirth Jivani <tirthjivani17@gmail.com>
2021-03-22 22:14:03 +0530

    logos entered and items updated

--- my_portfolio/src/App.jsx
+++ my_portfolio/src/App.jsx
@@ -3,21 +3,31 @@ import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import './App.css';
 import About from './pages/About';
 import Homepage from './pages/homepage';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import logo from './assets/logo.png';
 
 export default function App() {
   return (
     <Router>
       <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-          </ul>
-        </nav>
+        <Navbar fixed="sticky" bg="light" variant="light">
+          <Navbar.Brand href="#">
+            <img
+              alt=""
+              src={logo}
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+            />
+          </Navbar.Brand>
+          <Navbar.Collapse>
+            <Nav className="justify-content-end">
+              <Nav.Link href="/">Portfolio</Nav.Link>
+              <Nav.Link href="/about">About</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
 
         <Switch>
           <Route path="/about">
--- my_portfolio/src/pages/homepage.jsx
+++ my_portfolio/src/pages/homepage.jsx
@@ -1,3 +1,19 @@
-export default function Homepage() {
-  return <h2>Hello, I'm Tirth!</h2>;
+import React, { Component } from 'react';
+import { Button } from 'react-bootstrap';
+
+export default class Homepage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  render() {
+    return (
+      <>
+        <h2>Hello, I'm Tirth!</h2>
+        <Button variant="primary">Hii</Button>
+      </>
+    );
+  }
 }
