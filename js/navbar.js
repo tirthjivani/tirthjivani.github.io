9a310498e74e0517e7d165d54f9a9cd497e0f546
Tirth Jivani <tirthjivani17@gmail.com>
2021-03-29 11:42:32 +0530

    navbar issue fixed

--- js/navbar.js
+++ js/navbar.js
@@ -1,23 +1,20 @@
 $(document).ready(function() {
 	// var body = document.body, html = document.documentElement;
 	// var height = Math.max( body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight);
-	var s = $("#portfolio");
-	var c = $("#contact");
-    var n = $("#nav");
-	var pos = s.position();					   
+			   
 	$(window).scroll(function() {
         var windowpos = $(window).scrollTop();
-		if (windowpos >= pos.top & windowpos >= window.outerHeight) {
-            n.removeClass("transparent");
+		if (windowpos >= $("#portfolio").position().top & windowpos >= window.outerHeight) {
+            $("#nav").removeClass("transparent");
 		} else {
-			n.addClass("transparent");	
+			$("#nav").addClass("transparent");	
 		}
 
-		console.log(c.position().top-10, windowpos)
-		if (windowpos >= c.position().top-10) {
-            n.addClass("black-nav");
+		console.log($("#contact").position().top-10, windowpos)
+		if (windowpos >= $("#contact").position().top-10) {
+            $("#nav").addClass("black-nav");
 		} else {
-			n.removeClass("black-nav");	
+			$("#nav").removeClass("black-nav");	
 		}
 	});
 });
