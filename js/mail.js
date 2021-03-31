7446c956e357aabdfa5874879a1ff0ef622f082f
Tirth Jivani <tirthjivani17@gmail.com>
2021-03-31 12:38:50 +0530

    mail working

--- js/mail.js
+++ js/mail.js
@@ -1,3 +1,25 @@
+let form = document.getElementById("sendmail");
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+});
+
 function sendMail() {
-    console.log()
-} 
+    let name = form.elements[0];
+    let email = form.elements[1];
+    let msg = form.elements[2];
+	Email.send({
+		Host: "smtp.gmail.com",
+		Username: "hello.tirth.jivani@gmail.com",
+		Password: "Tirth#@!17",
+		To: email.value + ",tirthjivani17@gmail.com",
+		From: "hello.tirth.jivani@gmail.com",
+		Subject: "Hello from " + name.value + ".",
+		Body: msg.value,
+    }).then(() => {
+        name.value = "";
+        msg.value = "";
+        email.value = "";
+        msgText = "Email Successfully sent!";
+    })
+}
