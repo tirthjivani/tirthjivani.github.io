4318d2c225596a69a411409f7c6d0d8a78e4c3e7
Tirth <spurge@Tirths-15-MacBook-Pro.local>
2023-09-10 18:41:56 +0530

    updating images

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,43 @@
-const cursor = document.querySelector('cursor-fancy');
+gsap.set(".cursor", { xPercent: -50, yPercent: -50 });
 
+let cursor = document.querySelector(".cursor");
+let hand = document.querySelector(".hand");
+// let title = document.querySelector("h1");
 
-const positionElement = (e)=> {
-  const mouseY = e.clientY;
-  const mouseX = e.clientX;
+let mouseX;
+let mouseY;
 
-  cursor.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
-}
+window.addEventListener("mousemove", (e) => {
+  mouseX = e.clientX;
+  mouseY = e.clientY;
 
-window.addEventListener('mousemove', positionElement)
+  gsap.to(cursor, 0.5, { x: mouseX, y: mouseY });
+});
+
+// title.addEventListener("mouseenter", () => {
+//   gsap.to(hand, 1, {
+//     scale: 1,
+//     opacity: 1,
+//     top: "-75px",
+//     left: "-75px",
+//     rotate: 0,
+//     ease: Elastic.easeOut.config(1, 0.3),
+//   });
+// });
+
+// title.addEventListener("mousemove", () => {
+//   gsap.to(hand, 1, {
+//     x: mouseX,
+//     y: mouseY,
+//   });
+// });
+
+// title.addEventListener("mouseleave", () => {
+//   gsap.to(hand, 0.2, {
+//     scale: 0,
+//     opacity: 0,
+//     top: "10",
+//     left: "40",
+//     rotate: 45,
+//   });
+// });
