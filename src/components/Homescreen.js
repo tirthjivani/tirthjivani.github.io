35839c7d1316da7bea73e2a1bb814e331d9fd89e
Me, Tirth <tirthjivani17@gmail.com>
2024-08-25 01:20:06 +0530

    image updated

--- src/components/Homescreen.js
+++ src/components/Homescreen.js
@@ -5,12 +5,13 @@ import About from './About';
 import Project from './Project';
 import Footer from './Footer';
 import { motion } from 'framer-motion';
-import me from '../assets/img/me.png';
-import loot from '../assets/img/projects/loot.png';
-import petpuja from '../assets/img/projects/petpuja.png';
-import afterglow from '../assets/img/projects/afterglow.png';
-import mooddl from '../assets/img/projects/mooddl.png';
-import services from '../assets/img/projects/sellerapp-service.png';
+import me from '../assets/img/me.webp';
+import loot from '../assets/img/projects/loot.webp';
+import petpuja from '../assets/img/projects/petpuja.webp';
+import afterglow from '../assets/img/projects/afterglow.webp';
+import mooddl from '../assets/img/projects/mooddl.webp';
+import services from '../assets/img/projects/sellerapp-service.webp';
+import designSystem from '../assets/img/projects/design-system.webp';
 
 export default function Homescreen() {
   const handleContextMenu = (event) => {
@@ -38,13 +39,16 @@ export default function Homescreen() {
                 <em>UX</em>,<em>branding</em>, and <em>visual identities</em>.
               </h5>
               <Project
-                title={'LooT'}
+                title={'SellerApp Services'}
                 desc={
-                  'It is a coupon management platform for users to save and use all the coupon codes they have at one place.'
+                  'It was our first view to introduction of SellerApp Services in the dashboard for users to request.'
                 }
-                image={loot}
-                alt={'tirth-jivani-branding'}
-                link={'https://tome.app/tirthjivani/tirth-jivani-loot-case-study-cluzgmpuc0sfnod61bcuw31x6'}
+                image={services}
+                alt={'tirth-jivani-project-sellerapp-services'}
+                linkName={'Case Study'}
+                link={
+                  'https://www.figma.com/proto/iY4SvuHQ5UgIoPK2GTXDY3/Case-Studies?page-id=1%3A4&node-id=1-5&viewport=828%2C667%2C0.57&t=QeKxRH57a21KJyxP-8&scaling=min-zoom&content-scaling=fixed&disable-default-keyboard-nav=1&hotspot-hints=0&hide-ui=1'
+                }
               />
             </div>
             <div className="h-max w-full md:w-1/2 overflow-hidden rounded-3xl">
@@ -54,12 +58,10 @@ export default function Homescreen() {
 
           <div className="w-full flex flex-row">
             <Project
-              title={'SellerApp Services'}
-              desc={
-                'It was our first view to introduction of SellerApp Services in the dashboard for users to request.'
-              }
-              image={services}
-              alt={'tirth-jivani-project-sellerapp-services'}
+              title={'SellerApp Design System'}
+              desc={"Created and implimented SellerApp's Design System and increased 89% user engagement."}
+              image={designSystem}
+              alt={'tirth-jivani-project-sellerapp-design-system'}
               linkName={'Case Study'}
               link={
                 'https://www.figma.com/proto/iY4SvuHQ5UgIoPK2GTXDY3/Case-Studies?page-id=1%3A4&node-id=1-5&viewport=828%2C667%2C0.57&t=QeKxRH57a21KJyxP-8&scaling=min-zoom&content-scaling=fixed&disable-default-keyboard-nav=1&hotspot-hints=0&hide-ui=1'
@@ -76,15 +78,13 @@ export default function Homescreen() {
               link={'https://tirthjivani.myportfolio.com/mooddl'}
             />
             <Project
-              className={'h-full'}
-              title={'Afterglow'}
+              title={'LooT'}
               desc={
-                'This app was designed for interior design items, that focused on how a user, who was color blind, partially blind, or completely blind, uses an e-commerce app.'
+                'It is a coupon management platform for users to save and use all the coupon codes they have at one place.'
               }
-              image={afterglow}
-              alt={'tirth-jivani-project-afterglow'}
-              linkName={'Case Study'}
-              link={'https://tome.app/tirthjivani/tirth-jivani-afterglow-case-study-cluygcfe00evepl615yuu69i2'}
+              image={loot}
+              alt={'tirth-jivani-branding'}
+              link={'https://tome.app/tirthjivani/tirth-jivani-loot-case-study-cluzgmpuc0sfnod61bcuw31x6'}
             />
           </div>
 
